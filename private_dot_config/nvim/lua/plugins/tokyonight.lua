51d7bfb2f196cbbaa3c9e111c234dc827b984290
Joaquim Dos Santos <joaquim>
2025-01-18 00:11:00 +0100

    Tokyonight configuration

--- private_dot_config/nvim/lua/plugins/tokyonight.lua
+++ private_dot_config/nvim/lua/plugins/tokyonight.lua
@@ -1,22 +1,17 @@
 return {
   "folke/tokyonight.nvim",
-  -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  -- priority = 1000, -- make sure to load this before all the other start plugins
-  -- enabled = true,
-  -- config = function()
-  -- 	require("tokyonight").setup({
-  -- 		style = "night",
-  -- 		transparent = true,
-  -- 		terminal_colors = true,
-  -- 	})
-  -- 	vim.cmd([[colorscheme tokyonight]])
-  -- end,
-  opts = {
-    style = "night",
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
-    },
-  },
+  config = function()
+    require("tokyonight").setup({
+      style = "night",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_highlights = function(hl, c)
+        -- Set `fg` to the color you want your window separators to have
+        hl.WinSeparator = { fg = "white", bold = false }
+      end,
+    })
+  end,
 }
